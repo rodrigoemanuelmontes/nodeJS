71ec59539b44cb5dfea29169c1e3c27b76a8edd6
Rodrigo Montes <rodrigomontes167@gmail.com>
2025-04-25 10:00:58 -0300

    Modifico el ejercicio de la clase 04 (tarea). Comienzo la clase 05 sobre gestores de paquetes: creo una nueva carpeta, uso 'npm install' para instalar 'nodemailer' (envío de emails) y 'nodemon' (entorno de desarrollo). En el package.json agrego el script 'start: node gestores.js' para ejecutar con 'npm run start' o 'npm start'.

---
+++ GESTORES/gestores.js
@@ -0,0 +1,45 @@
+
+// Un gestor de paquetes funciona como una biblioteca de módulos o paquetes de código de 
+// programación donde a su vez eliminan la necesidad de descargar e integrar manualmente 
+// estas bibliotecas, lo que simplifica el desarrollo y asegura que las dependencias estén 
+// organizadas y actualizadas. 
+// Estas herramientas que facilitan la instalación, actualización y gestión de bibliotecas o 
+// dependencias en un proyecto. Estas dependencias son piezas de código que otros 
+// desarrolladores han creado para resolver problemas comunes, como conectarse a una base 
+// de datos, realizar peticiones HTTP o manejar archivos. 
+
+// ¿Para qué sirven los gestores de paquetes? 
+
+// ●  Instalar dependencias de terceros. 
+// ●  Gestionar versiones de esas dependencias. 
+// ●  Actualizar y eliminar dependencias de forma sencilla. 
+// ●  Resolver la integración e interacción entre las distintas dependencias y las versiones 
+// de las subdependencias que tienen instaladas. 
+// El concepto de dependencias, paquetes o módulos es propio de la programación en 
+// general, incluso existen gestores de paquetes para instalar programas en los distintos 
+// sistemas operativos. Conozcamos algunos de ellos: 
+// NPM 
+// Node Package Manager o gestor de paquetes de Node JS, podría considerarse el más 
+// conocido y utilizado, sin embargo existen 
+// alternativas como yarn o pnpm. Cuanto más 
+// alcance tiene un gestor de paquetes más probable 
+// es que la dependencia o librería que estés 
+// necesitando haya sido subida ahí por el 
+// desarrollador. 
+// Homebrew 
+// Permite manejar programas del sistema operativo Mac OS presente en las 
+// computadoras marca Apple.  
+ 
+// Composer 
+// Usado en proyectos del lenguaje de programación PHP. 
+ 
+// Chocolatey 
+// El gestor de paquetes del sistema operativo Microsoft nos permite instalar 
+// infinidad de programas desde la terminal sin tener que descargar los ejecutables. 
+// Pip 
+// Gestor de paquetes para el lenguaje Python. 
+ 
+// Con los gestores de paquetes, podemos obtener enormes ventajas desde el acceso a miles 
+// de librerías o módulos de terceros hasta un entorno estandarizado de configuración.    
+ 
+console.log("¿Para qué sirven los gestores de paquetes? ||Instalar dependencias de terceros ||  Gestionar versiones de esas dependencias ||Actualizar y eliminar dependencias de forma sencilla || Resolver la integración e interacción entre las distintas dependencias y las versiones las subdependencias que tienen instaladas");
